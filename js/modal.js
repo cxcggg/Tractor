ca60ea88e9dcdf8b8331a4baae1ae13a31b011c6
minor-57 <agapovdanya@yandex.ru>
2024-01-19 12:34:12 +0300

    call-back modal window

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+function addListeners() {
+    const btn = document.querySelector('.btn__main-call')
+    const modalWindow = document.getElementById('call-back');
+    const clickClose = document.querySelector('.click-close');
+    const btnClose = document.querySelector('.modal__close');
+
+    btn.addEventListener('click', () => {
+        modalWindow.classList.toggle('active');
+        clickClose.classList.toggle('active');
+        document.body.classList.add('no-scrol');
+    })
+
+    clickClose.addEventListener('click', () => {
+        modalWindow.classList.remove('active');
+        clickClose.classList.remove('active');
+        document.body.classList.remove('no-scrol');
+    })
+
+    btnClose.addEventListener('click', () => {
+        modalWindow.classList.remove('active');
+        clickClose.classList.remove('active');
+        document.body.classList.remove('no-scrol');
+    })
+}
+
+window.addEventListener('DOMContentLoaded', addListeners)
